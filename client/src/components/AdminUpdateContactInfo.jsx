5298acd1f7d3a8975f0f94a8552301c7ddf6d727
Chandan1952 <chandanpandit754@gmail.com>
2025-03-29 11:19:55 +0530

    Update AdminUpdateContactInfo.jsx

--- client/src/components/AdminUpdateContactInfo.jsx
+++ client/src/components/AdminUpdateContactInfo.jsx
@@ -17,7 +17,7 @@ const UpdateContactInfo = () => {
     useEffect(() => {
       const verifyAdminSession = async () => {
         try {
-          const response = await axios.get("https://shopping-portal-wptg.onrender.com/admin-verify", { withCredentials: true });
+          const response = await axios.get("https://shopping-portal-backend.onrender.com/admin-verify", { withCredentials: true });
           if (!response.data.isAdmin) {
             navigate("/admin-login", { replace: true });
           }
@@ -33,7 +33,7 @@ const UpdateContactInfo = () => {
 
   useEffect(() => {
     // Fetch current contact details
-    fetch("https://shopping-portal-wptg.onrender.com/contact-details")
+    fetch("https://shopping-portal-backend.onrender.com/contact-details")
       .then((response) => response.json())
       .then((data) => setContactInfo(data))
       .catch((error) => console.error("Error fetching contact info:", error));
@@ -44,7 +44,7 @@ const UpdateContactInfo = () => {
   };
 
   const handleUpdate = () => {
-    fetch("https://shopping-portal-wptg.onrender.com/update-contact", {
+    fetch("https://shopping-portal-backend.onrender.com/update-contact", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(contactInfo),
