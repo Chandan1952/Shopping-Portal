44c7cc547b2d6fcc0ff88fddec978d15cb68cda4
Chandan1952 <chandanpandit754@gmail.com>
2025-03-29 11:15:27 +0530

    Update ForgotPassword.jsx

--- client/src/components/ForgotPassword.jsx
+++ client/src/components/ForgotPassword.jsx
@@ -15,7 +15,7 @@ const ForgotPassword = () => {
         setLoading(true);
 
         try {
-            const response = await fetch("https://shopping-portal-wptg.onrender.com/forgot-password", {
+            const response = await fetch("https://shopping-portal-backend.onrender.com/forgot-password", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
