cd3516809b341f222be6ffaf6e7fee0153ad1ff9
Chandan1952 <chandanpandit754@gmail.com>
2025-05-30 19:59:58 +0530

    Update Discover.jsx

--- client/src/components/Discover.jsx
+++ client/src/components/Discover.jsx
@@ -17,7 +17,7 @@ export default function Discover() {
   useEffect(() => {
     setIsLoading(true);
     axios
-      .get("https://myntra-clone-api.vercel.app/carousel-images")
+      .get("https://shopping-portal-backend.onrender.com/carousel-images")
       .then((res) => {
         setImages(res.data.images);
         setIsLoading(false);
